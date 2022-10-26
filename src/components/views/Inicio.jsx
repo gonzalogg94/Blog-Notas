6cf299c4e0b6feff84d720bef9d775e34730159d
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-26 20:02:09 -0300

    termine la funcionaliad del ejercicio 14

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Container,Row } from 'react-bootstrap';
 import CardReceta from './Receta/CardReceta';
+import { useState,useEffect } from 'react';
+import { consultarAPI } from '../helpers/queries';
 
 const Inicio = () => {
+    const [recetas, setRecetas] = useState([]);
+  useEffect(()=>{
+
+    // opcion 1
+    consultarAPI().then((respuesta)=>{
+      console.log(respuesta)
+      setRecetas(respuesta)
+    })
+},[])
     return (
        <Container className='mainSection py-4'>
         <h1 className='display-2 text-center text-white'>Lista De Recetas</h1>
         <hr className='text-white' />
         <Row xs={1} md={4} className="g-4">
-            <CardReceta></CardReceta>
+            {
+                recetas.map((producto)=> <CardReceta key={producto.id} producto={producto} setProductos={setRecetas}></CardReceta> )
+
+            }
       </Row>
        </Container>
     );
--- src/components/views/Receta/CardReceta.jsx
+++ src/components/views/Receta/CardReceta.jsx
@@ -1,19 +1,23 @@
+import { Badge } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
-import Card from 'react-bootstrap/Card';
-
-const CardReceta = () => {
-    return (
-        <Card className='mt-5 bg-black text-white'>
-      <Card.Img className='pt-3' variant="top" src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-      <Card.Body className='text-center'>
-        <Card.Title>Titulo</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+const CardReceta = ({ producto }) => {
+  return (
+    <Card className="mt-5 bg-black text-white">
+      <Card.Img
+        className="pt-3"
+        variant="top"
+        src={producto.imagen}
+      />
+      <Card.Body className="text-center">
+        <Card.Title>{producto.nombreReceta}</Card.Title>
+        <Card.Text>{producto.descripcion}</Card.Text>
+        <Badge bg="warning" text="dark">
+        {producto.categoria}
+      </Badge>
       </Card.Body>
     </Card>
-    );
+  );
 };
 
 export default CardReceta;
